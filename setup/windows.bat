c74261ade210c7a30d04cbb949a47265429e1784
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2025-08-23 13:51:57 -0400

    ok.

--- setup/windows.bat
+++ setup/windows.bat
@@ -14,9 +14,9 @@ haxelib install hscript --quiet
 haxelib install hxcpp-debug-server --quiet
 haxelib git away3d https://github.com/moxie-coder/away3d --quiet
 haxelib git tjson https://github.com/moxie-coder/tjson --quiet
-haxelib git hxcpp https://github.com/th2l-devs/hxcpp --quiet
+haxelib git hxcpp https://github.com/HaxeFoundation/hxcpp --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
-haxelib git linc_luajit https://github.com/th2l-devs/linc_luajit --quiet
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit --quiet
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git --quiet
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc --quiet
